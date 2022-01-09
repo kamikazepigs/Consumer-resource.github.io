9ef9daef29e2340513fab39d80b051479d4d6066
kamikazepigs <97355059+kamikazepigs@users.noreply.github.com>
2022-01-09 20:35:36 +0100

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -14,5 +14,53 @@ ideologies = [
             "stra": 0,
             "mate": 100,
         }
+    },
+        {
+        "name": "Lectivore",
+        "stats": {
+            "nutr": 100,
+            "stra": 0,
+            "mate": 0,
+        }
+    },
+        {
+        "name": "Carcassivore",
+        "stats": {
+            "nutr": 0,
+            "stra": 0,
+            "mate": 0,
+        }
+    },
+        {
+        "name": "Zontanophage",
+        "stats": {
+            "nutr": 100,
+            "stra": 100,
+            "mate": 100,
+        }
+    },
+        {
+        "name": "Sarcophage",
+        "stats": {
+            "nutr": 0,
+            "stra": 100,
+            "mate": 100,
+        }
+    },
+        {
+        "name": "Thanatophages",
+        "stats": {
+            "nutr": 100,
+            "stra": 100,
+            "mate": 0,
+        }
+    },
+        {
+        "name": "Necrophage",
+        "stats": {
+            "nutr": 0,
+            "stra": 100,
+            "mate": 0,
+        }
     }
 ];
