5784e404cbfdb0041a61eb5452685f1ba7a8dd40
kamikazepigs <97355059+kamikazepigs@users.noreply.github.com>
2022-01-10 21:21:04 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -46,5 +46,101 @@ questions = [
             "stra": 0,
             "mate": 0,
         }
-    }
+    },
+    {
+        "question": "I like the idea of hunting/gathering my own food.",
+        "effect": {
+            "nutr": 0,
+            "stra": 0,
+            "mate": 20,
+        }
+    },
+    {
+    "question": "I prefear fresh food rather than aged food.",
+        "effect": {
+            "nutr": 0,
+            "stra": 0,
+            "mate": 20,
+        }
+    },
+    {
+    "question": "I wouldn't eat rotting food.",
+        "effect": {
+            "nutr": 0,
+            "stra": 0,
+            "mate": 10,
+        }
+    },
+    {
+    "question": "I someone dosen't finish teir food I will gladly eat it.",
+        "effect": {
+            "nutr": 0,
+            "stra": 0,
+            "mate": -10,
+        }
+    },
+    {
+    "question": "There is nothing bad from eating roadkills or food found in the trash, even if it's gone bad.",
+        "effect": {
+            "nutr": 0,
+            "stra": 0,
+            "mate": -20,
+        }
+    },
+    {
+    "question": "I would rather eat actual trash than eating food that has been just killed/picked.",
+        "effect": {
+            "nutr": 0,
+            "stra": 0,
+            "mate": -20,
+        }
+    },
+    {
+    "question": "I dream about living in a world made of food.",
+        "effect": {
+            "nutr": 0,
+            "stra": 10,
+            "mate": 0,
+        }
+    },
+    {
+    "question": "I envy the way worms can really dig into their meals.",
+        "effect": {
+            "nutr": 0,
+            "stra": 20,
+            "mate": 0,
+        }
+    },
+    {
+    "question": "There modern way of eating is too much ritualistic, we should eat in a way that emphasize more on getting the nutrients and less in the act of eating itself.",
+        "effect": {
+            "nutr": 0,
+            "stra": 20,
+            "mate": 0,
+        }
+    },
+    {
+    "question": "When I eat I really like to grab, see and bite the food I'm eating.",
+        "effect": {
+            "nutr": 0,
+            "stra": -10,
+            "mate": 0,
+        }
+    },
+    {
+    "question": "The idea of being covered in food is almost disgusting to me.",
+        "effect": {
+            "nutr": 0,
+            "stra": -20,
+            "mate": 0,
+        }
+    },
+    {
+    "question": "I would never give up taste just for a more efficient way of eating.",
+        "effect": {
+            "nutr": 0,
+            "stra": -20,
+            "mate": 0,
+        }
+    },
 ];
