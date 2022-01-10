5e2623cf4aaa02427a2b44776590745f89e54eee
kamikazepigs <97355059+kamikazepigs@users.noreply.github.com>
2022-01-10 20:39:09 +0100

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,50 @@
+questions = [
+    {
+        "question": "I consider myself a vegan or something like it.",
+        "effect": {
+            "nutr": 20,
+            "stra": 0,
+            "mate": 0,
+        }
+    },
+    {
+        "question": "I would never eat meat, and if I would it would probably hurt me.",
+        "effect": {
+            "nutr": 20,
+            "stra": 0,
+            "mate": 0,
+        }
+    }
+      {
+        "question": "I consider myself carnivorous or something like it.",
+        "effect": {
+            "nutr": -20,
+            "stra": 0,
+            "mate": 0,
+        }
+    }
+          {
+        "question": "I don't eat plants and fruits, they just don't give me the right nutrients.",
+        "effect": {
+            "nutr": -20,
+            "stra": 0,
+            "mate": 0,
+        }
+    }
+          {
+        "question": "I like fruits and vegetables.",
+        "effect": {
+            "nutr": 10,
+            "stra": 0,
+            "mate": 0,
+        }
+    }
+          {
+        "question": "I like meat.",
+        "effect": {
+            "nutr": 10,
+            "stra": 0,
+            "mate": 0,
+        }
+    }
+];
